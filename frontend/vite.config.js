9040172a7adbd417a31b4c657be988818400ba1b
JimmyRecce <jack.charlesworth@proton.me>
2025-10-01 18:11:04 +0100

    Fixed frontend server not listening to the correct address

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
+        server: {
+            host: "0.0.0.0",
+            port: 5173,
+        },
         proxy:{
             '/api/':{
-                target: 'http://localhost:7000',
+                target: 'http://backend:7000',
                   chargeOrigin:true,
             },  
         },
