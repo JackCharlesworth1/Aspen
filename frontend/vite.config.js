4f0b0159b073faf0a79f2fb0dda5bfc46dca21ca
JimmyRecce <jack.charlesworth@proton.me>
2025-10-01 18:16:34 +0100

    Fixed accidental server redefinition in configfile

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-        server: {
-            host: "0.0.0.0",
-            port: 5173,
-        },
+        host: "0.0.0.0",
+        port: 5173,
         proxy:{
             '/api/':{
                 target: 'http://backend:7000',
