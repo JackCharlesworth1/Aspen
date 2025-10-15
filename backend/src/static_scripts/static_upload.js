f004b4daa6ee4ea27638e3ccc15c45e2596c3547
JimmyRecce <jack.charlesworth@proton.me>
2025-10-15 17:01:25 +0100

    Corrected typo in last fix

--- backend/src/static_scripts/static_upload.js
+++ backend/src/static_scripts/static_upload.js
@@ -23,7 +23,7 @@ async function addAudioToSpecies(req,res,name){
 async function addSightingToUser(req,res,username,species_name,number,image_data){
     const filename=number.toString()+".jpg"
     const __dirname=import.meta.dirname
-    const filepath=path.join(__dirname,'..','..','static','user',username,'images',species_name.toLowerCase().replace("_"," "),filename)
+    const filepath=path.join(__dirname,'..','..','static','user',username,'images',species_name.toLowerCase().replace(" ","_"),filename)
     console.log("Writing Sightings To",filepath)
     await fs.writeFile(filepath,req.file.buffer,err=>{
         if(err) return new Error("Failed to copy file from buffer to permenant storage");
