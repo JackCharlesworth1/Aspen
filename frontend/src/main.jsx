b862d858476b9d4ddf58d2b034d37a4cbcd1644c
JimmyRecce <jack.charlesworth@proton.me>
2025-11-02 12:01:53 +0000

    Temporarily removed strict mode to aid in logging attempts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,14 +7,13 @@ import './css/global.css'
 
 const VITE_GOOGLE_CLIENT_ID=import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
+console.log("Environment variables:",import.meta.env)
 console.log("VITE_GOOGLE_CLIENT_ID:",VITE_GOOGLE_CLIENT_ID)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <GoogleOAuthProvider clientId={VITE_GOOGLE_CLIENT_ID}>
         <AuthProvider>
             <App />
         </AuthProvider>
     </GoogleOAuthProvider>
-  </StrictMode>,
 )
