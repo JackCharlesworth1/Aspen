d161cd4dda13de44107461db8baf517e85b1e9d8
JimmyRecce <jack.charlesworth@proton.me>
2025-11-03 09:59:10 +0000

    Changed allowed cors methods to be more inclusive, hopefully stopping it blocking certain requests

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(cors({
     }
     return callback(null, true);
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }))
