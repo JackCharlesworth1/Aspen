2198f4de6b44bb0629390ac66f52b2236ab22922
JimmyRecce <jack.charlesworth@proton.me>
2025-11-03 20:40:31 +0000

    Personalised the account screen to ensure a firm starting point

--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -1,6 +1,31 @@
+import {useState,useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
+
 const AccountPage=()=>{
+    const navigate=useNavigate();
+    const [username,setUsername]=useState(null);
+
+    const fetchAccountInfo=()=>{
+        const percieved_username=localStorage.getItem("client_percieved_username")
+        const token=localStorage.getItem("accessToken")
+        setUsername(percieved_username);
+    }
+
+    const userClickedToLogout=()=>{
+        localStorage.setItem("accessToken","")
+        localStorage.setItem("client_percieved_user_level","")
+        localStorage.setItem("client_percieved_username","")
+        navigate("/landing/")
+    }
+
+    useEffect(()=>{
+       fetchAccountInfo(); 
+    },[username])
+
     return(
-        <h3>Account Page</h3>
+        <h1>Account</h1>
+        {username&&<h3>{username}</h3>}
+        <button onClick={userClickedToLogout}>Log Out</button>
     )
 }
 export default AccountPage
