0a361d34d56463ef8bbffb274907cf58cb581e78
JimmyRecce <jack.charlesworth@proton.me>
2025-11-05 17:29:46 +0000

    Added some stats to make the account screen more personalised

---
+++ frontend/src/components/UserStats.jsx
@@ -0,0 +1,126 @@
+import {useState,useEffect} from 'react'
+const UserStats=({user_seen,user_sightings})=>{
+
+	const [speciesData,setSpeciesData]=useState([]);
+	const [tags,setTags]=useState([])
+	const [sortedSeenSpecies,setSortedSeenSpecies]=useState([]);
+	const [mixedSightings,setMixedSightings]=useState([]);
+	
+	const prepareUserStats=()=>{
+		sortSeenSpecies();
+		mixSightings();
+	}
+
+	const sortSeenSpecies=()=>{
+		const token=localStorage.getItem("accessToken")
+		const species_data_response=await fetch("https://api.theaspenproject.cloud/api/species/",{headers:{"Authorization":token}});
+		const tags_response=await fetch("https://api.theaspenproject.cloud/api/species/tags/",{headers:{"Authorization":token}});
+		if(!(species_data_response.status===200&&tag_response.status===200)){
+			return;
+		}
+		const species_data=await species_data_response.json()
+		const tags_data=await tags_response.json();
+		setSpeciesData(species_data)
+		setTags(tags_data);
+		if(user_seen){
+			const tag_seen_count={}
+			for(let i=0;i<tags_data.length;i++){
+				tag_seen_count[tags_data[i]]=0;
+			}
+			for(let j=0;j<user_seen.length;j++){
+				const species_object_to_index=null;
+				for(let k=0;k<species_data.length;k++){
+					if(species_data[k].SpeciesName===user_seen[j]){
+						species_object_to_index=speciesData[k]
+					}
+				}
+				for(let l=0;l<tags_data.length;l++){
+					if(species_object_to_index.SpeciesTags.includes(tags_data[l])){
+						tags_seen_count[tags_data[l]]=tags_seen_count[tags_data[l]]+1;	
+					}
+				}
+			}
+			const sorted_tag_data=[];
+			sorted_tag_data.push(tags_data[0])
+			for(let m=1;m<tags_data.length;m++){
+				let assumed_index=0;
+				while(tag_seen_count[sorted_tag_data[assumed_index]]>tag_seen_count[tag_data[m]]){
+					assumed_index++;
+				}
+				if(assumed_index===sorted_tag_data.length){
+					sorted_tag_data.push(tag_data[m])
+				}else{
+					sorted_tag_data.insert(assumed_index,tag_data[m])
+				}
+			}
+			setSortedSeenSpecies(sorted_tag_data)
+		}
+		
+	}
+
+
+	const mixSightings=()=>{
+		user_sightings_count={}
+		for(let i=0;i<user_sightings.length;i++){
+			if(user_sightings_count.keys().includes(user_sightings[i])){
+				user_sightings_count[user_sightings[i]]=user_sightings_count[user_sightings[i]]+1
+			}else{
+				user_sightings_count[user_sightings[i]]=1;
+			}	
+		}
+		const sorted_sightings=[];
+		sorted_sightings.push(user_sightings[0])
+		for(let j=0;j<user_sightings_count.keys().length;j++){
+			let assumed_index=0;	
+			while(user_sightings_count[sorted_sightings[assumed_index]]>user_sightings_count[user_sightings_count.keys()[j]]){
+				assumed_index++
+			}
+			if(assumed_index===sorted_sightings.length){
+				sorted_sightings.push(user_sightings_count.keys()[j])
+			}else{
+				sorted_sightings.insert(assumed_index,user_sightings_count.keys()[j])
+			}
+		}
+		const ordered_sightings=[]
+		for(let k=0;k<user_sightings_count[sorted_sightings[0]];k++){
+			ordered_sightings.push(sorted_sightings[0]);
+		}
+		for(let l=1;l<sorted_sightings.length;l++){
+			const places_per_interval=Math.floor(ordered_sightings.length/user_sightings_count[sorted_sightings[l]])
+			for(let m=user_sightings_count[sorted_sightings[l]];m>0;m--){
+				if(places_per_interval*m>=ordered_sightings.length){
+					ordered_sightings.push(sorted_sightings[l])
+				}else{
+					ordered_sightings.insert(places_per_interval*m,sorted_sightings[l])
+				}
+			}
+		}
+		setMixedSightings(ordered_sightings)
+		
+	}
+
+	useEffect(()=>{prepareUserStats()},[user_seen,user_sightings])
+	return (<>
+			{(sortedSeenSpecies.length>0)&&<div>
+				<h5>What you see the most:</h5>
+				<ol>
+					{(sortedSeenSpecies.length>0)&&<li>sortedSeenSpecies[0]</li>}
+					{(sortedSeenSpecies.length>1)&&<li>sortedSeenSpecies[1]</li>}
+					{(sortedSeenSpecies.length>2)&&<li>sortedSeenSpecies[2]</li>}
+				</ol>
+			</div>}
+			{(sortedSeenSpecies.length>4)&&<div>
+				<h5>What you see least:</h5>
+				<ol>
+					{(sortedSeenSpecies.length>4)&&<li>sortedSeenSpecies[sortedSeenSpecies.length-1]</li>}
+					{(sortedSeenSpecies.length>5)&&<li>sortedSeenSpecies[sortedSeenSpecies.length-2]</li>}
+					{(sortedSeenSpecies.length>6)&&<li>sortedSeenSpecies[sortedSeenSpecies.length-6]</li>}
+				</ol>
+			</div>}
+
+
+		</>)
+}
+
+export default UserStats
+
--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -1,9 +1,12 @@
 import {useState,useEffect} from 'react'
 import {useNavigate} from 'react-router-dom'
+import UserStats from '../components/UserStats.jsx'
 
 const AccountPage=()=>{
     const navigate=useNavigate();
     const [username,setUsername]=useState(null);
+    const [userSeen,setUserSeen]=useState([]);
+    const [userSightings,setUserSightings]=useState([]);
 
     const fetchAccountInfo=async ()=>{
         let percieved_username=localStorage.getItem("client_percieved_username")
@@ -15,6 +18,12 @@ const AccountPage=()=>{
         const user_details_response=await fetch("https://api.theaspenproject.cloud/api/account/info/"+percieved_username)
         if((!user_details_response.Error)&&user_details_response.status===200){
             user_details_data=await user_details_response.json()
+	    if(user_details_data.sightings){
+		setUserSightings(user_details_data.sightings)
+	    }
+	    if(user_details_data.seen){
+		setUserSeen(user_details_data.seen)
+	    }
             console.log("Got Info About User",user_details_data)
         }else{
             console.log("An Error Occured Fetching Account Info:",user_details_response.status,user_details_response.Error)
@@ -36,6 +45,7 @@ const AccountPage=()=>{
         <div>
             <h1>Account</h1>
             {username&&<h3>{username}</h3>}
+	    <UserStats user_seen={userSeen} user_sightings={userSightings}/>
             <button onClick={userClickedToLogout}>Log Out</button>
         </div>
     )
