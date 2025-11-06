19ac21dadd98fb02068cd7259b1e8165386235a2
JimmyRecce <jack.charlesworth@proton.me>
2025-11-06 12:58:53 +0000

    Changed gallery data formatting to allign with application convention

--- frontend/src/components/UserStats.jsx
+++ frontend/src/components/UserStats.jsx
@@ -88,7 +88,7 @@ const UserStats=({actual_username,user_seen,user_sightings})=>{
 		const ordered_sightings=[]
         if(sorted_sightings.length!==0){
 		    for(let k=0;k<user_sightings_count[sorted_sightings[0]];k++){
-			    ordered_sightings.push("https://api.theaspenproject.cloud/api/static/user/"+actual_username.replace(" ","_")+"/images/"+sorted_sightings[0].replace(" ","_").toLowerCase()+"/"+k.toString()+".jpg");
+			    ordered_sightings.push("https://api.theaspenproject.cloud/api/static/user/"+actual_username.replace(" ","_").toLowerCase()+"/images/"+sorted_sightings[0].replace(" ","_").toLowerCase()+"/"+k.toString()+".jpg");
 		    }
         }
         if(sorted_sightings.length>1){
@@ -96,9 +96,9 @@ const UserStats=({actual_username,user_seen,user_sightings})=>{
 			    const places_per_interval=Math.ceil((ordered_sightings.length-1)/user_sightings_count[sorted_sightings[l]])
 			    for(let m=user_sightings_count[sorted_sightings[l]];m>0;m--){
 				    if(places_per_interval*m>=ordered_sightings.length){
-					    ordered_sightings.push("https://api.theaspenproject.cloud/api/static/user/"+actual_username.replace(" ","_")+"/images/"+sorted_sightings[l].replace(" ","_").toLowerCase()+"/"+(m-1).toString()+".jpg")
+					    ordered_sightings.push("https://api.theaspenproject.cloud/api/static/user/"+actual_username.replace(" ","_").toLowerCase()+"/images/"+sorted_sightings[l].replace(" ","_").toLowerCase()+"/"+(m-1).toString()+".jpg")
 				    }else{
-					    ordered_sightings.splice(places_per_interval*m,0,"https://api.theaspenproject.cloud/api/static/user/"+actual_username.replace(" ","_")+"/images/"+sorted_sightings[l].replace(" ","_").toLowerCase()+"/"+(m-1).toString()+".jpg")
+					    ordered_sightings.splice(places_per_interval*m,0,"https://api.theaspenproject.cloud/api/static/user/"+actual_username.replace(" ","_").toLowerCase()+"/images/"+sorted_sightings[l].replace(" ","_").toLowerCase()+"/"+(m-1).toString()+".jpg")
 				    }
 			    }
 		    }
