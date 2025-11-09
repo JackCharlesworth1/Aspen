f53a695b993c76b921a3afc5c8114bc5900fd82c
JimmyRecce <jack.charlesworth@proton.me>
2025-11-09 14:35:08 +0000

    Fixed old typo

--- backend/src/controllers/external_controller.js
+++ backend/src/controllers/external_controller.js
@@ -11,7 +11,7 @@ const getNearbySpeciesLocationsHandler=async (req,res)=>{
     if(!(req.params.origin&&req.params.species)){
         return res.status(400).json({"message":"You have to submit both an origin and a species in order to determine the location of an origin at a species"})
     }
-    const get_locations_prompt='List the top 5 locations near '+req.body.origin+', where '+req.body.species+' are very likely to be found. The locations can be informal i.e. not a reserve, but you need to be specific, do not list an entire county/town or region. Output specific places. Think about what is a suitable habitat and work backwards. Only output the list as: "Placename1, Placename2, Placename3, Placename4, Placename5" Do not include any other text, explanation, or instructions. No line breaks. No numbering. Follow this format exactly. If names are generic e.g. meadows, use a more specific placename';
+    const get_locations_prompt='List the top 5 locations near '+req.params.origin+', where '+req.params.species+' are very likely to be found. The locations can be informal i.e. not a reserve, but you need to be specific, do not list an entire county/town or region. Output specific places. Think about what is a suitable habitat and work backwards. Only output the list as: "Placename1, Placename2, Placename3, Placename4, Placename5" Do not include any other text, explanation, or instructions. No line breaks. No numbering. Follow this format exactly. If names are generic e.g. meadows, use a more specific placename';
     const response=await openai.chat.completions.create({
         model: "gpt-4-turbo",
         messages: [
