ddf324efa9976fb281fcfa5605a383181871d511
JimmyRecce <jack.charlesworth@proton.me>
2025-11-16 16:18:20 +0000

    Fixed import/export issue

--- backend/src/controllers/external_controller.js
+++ backend/src/controllers/external_controller.js
@@ -74,4 +74,4 @@ const getReccomendationsHandler=async(req,res)=>{
 
 }
 
-export {getNearbySpeciesLocationsHandler,identifySpeciesHandler}
+export {getNearbySpeciesLocationsHandler,identifySpeciesHandler,getReccomendationsHandler}
