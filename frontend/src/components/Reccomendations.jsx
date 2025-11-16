5df9fa68d592de6fb3a65033a0c137a9cf60b0a5
JimmyRecce <jack.charlesworth@proton.me>
2025-11-16 16:05:52 +0000

    Fixed syntax issue

--- frontend/src/components/Reccomendations.jsx
+++ frontend/src/components/Reccomendations.jsx
@@ -1,4 +1,4 @@
-import {useState,useEffect} import 'react'
+import {useState,useEffect} from 'react'
 import SpeciesCard from './SpeciesCard.jsx'
 import styles from '../css/SpeciesTable.module.css'
 
